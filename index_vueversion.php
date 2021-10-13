0686a97e66c8017622309d46a0d06da44477e004
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-10-13 11:24:03 +0200

    font import

--- index_vueversion.php
+++ index_vueversion.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/vue"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js)"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style/style.css">
     <title>PHP Disc - Vue version</title>
 </head>
