c00594d96b3392504b21d008b916c5e0d6cb373b
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-10-13 12:49:45 +0200

    js comment

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ const app = new Vue(
         },
         created() {
             axios .get('http://localhost/Esercizi/php-ajax-dischi/api/server.php')
+            //POTEVO SCRIVERE ANCHE .get('api/server.php') perché la chiamata in realtà parte dall'index e quindi entro direttamente dalla cartella
             .then ((res) => {
                 console.log(res.data);
                 this.discs = res.data;
