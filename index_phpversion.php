2c92acc067a8ca07f8566c247d0e20872483d1b4
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-10-13 12:22:52 +0200

    title change

--- index_phpversion.php
+++ index_phpversion.php
@@ -12,7 +12,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style/style.css">
-    <title>PHP-Disc</title>
+    <title>PHP-Disc - PHP version</title>
 </head>
 <body>
     <header>
